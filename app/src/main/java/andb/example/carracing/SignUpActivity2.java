5305a7bfb110876c67a967d2f6b793f53442463c
Toan Nang <112840020+Le-Nguyen-Toan-Nang@users.noreply.github.com>
2024-05-24 08:52:32 +0700

    change Sign Up screen

--- app/src/main/java/andb/example/carracing/SignUpActivity2.java
+++ app/src/main/java/andb/example/carracing/SignUpActivity2.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,30 +15,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignInActivity2 extends AppCompatActivity implements View.OnClickListener {
+public class SignUpActivity2 extends AppCompatActivity implements View.OnClickListener{
+
     private EditText etUsername;
     private EditText etPassword;
-    private TextView tvNoAccount;
-    private Button btnSignin;
+    private EditText etConfirmPassword;
+    private TextView tvAlreadyAccount;
+    private Button btnSignUp;
     private final String REQUIRE = "Require";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_in2);
+        setContentView(R.layout.activity_sign_up2);
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
-        tvNoAccount = findViewById(R.id.tvNotAccountYet);
-        btnSignin = findViewById(R.id.btnSignIn);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
+        tvAlreadyAccount = findViewById(R.id.tvAlreadyAccount);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
-        tvNoAccount.setOnClickListener(this);
-        btnSignin.setOnClickListener(this);
+        tvAlreadyAccount.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
     }
     private boolean checkInput(){
-        String REQUIRE = "Require";
         if (TextUtils.isEmpty(etUsername.getText().toString())){
             etUsername.setError(REQUIRE);
             return false;
@@ -46,27 +47,34 @@ public class SignInActivity2 extends AppCompatActivity implements View.OnClickLi
             etPassword.setError(REQUIRE);
             return false;
         }
+        if (TextUtils.isEmpty(etConfirmPassword.getText().toString())){
+            etPassword.setError(REQUIRE);
+            return false;
+        }
+        if (!TextUtils.equals(etPassword.getText().toString(),
+                etConfirmPassword.getText().toString())){
+            Toast.makeText(this, "Password are not match", Toast.LENGTH_LONG).show();
+            return false;
+        }
         return true;
     }
-    private void signIn(){
-        if(!checkInput()){
+    private void signUp(){
+        if (!checkInput())
             return;
-        }
-
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
-    private void signUpForm() {
-        Intent intent = new Intent(this, SignUpActivity2.class);
+    private void signInForm(){
+        Intent intent = new Intent(this, SignInActivity2.class);
         startActivity(intent);
         finish();
     }
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btnSignIn)
-            signIn();
-        else if (v.getId() == R.id.tvNotAccountYet)
-            signUpForm();
+    public void onClick(View v){
+        if (v.getId() == R.id.btnSignUp)
+            signUp();
+        else if (v.getId() == R.id.tvAlreadyAccount)
+            signInForm();
     }
 }
